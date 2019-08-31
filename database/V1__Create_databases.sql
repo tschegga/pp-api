7d3d9d607e56a9aae2271de5dddd6a294403df8b
Silvan Haeussermann <silvan.haeussermann@gmail.com>
2019-08-31 19:07:36 +0200

    Added flyway support

---
+++ database/V1__Create_databases.sql
@@ -0,0 +1,24 @@
+CREATE TABLE users (
+    users_id INT AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    password VARCHAR(32) NOT NULL,
+    role INT NOT NULL,
+    PRIMARY KEY (users_id)
+);
+
+CREATE TABLE sessions (
+    sessions_id INT AUTO_INCREMENT,
+    start VARCHAR(255) NOT NULL,
+    end VARCHAR(255) NOT NULL,
+    quality INT NOT NULL,
+    PRIMARY KEY (sessions_id)
+);
+
+CREATE TABLE roles (
+    roles_id INT AUTO_INCREMENT,
+    name VARCHAR(255),
+    PRIMARY KEY (roles_id)
+);
+
+INSERT INTO roles(name) VALUES ('admin');
+INSERT INTO roles(name) VALUES ('athlete');
